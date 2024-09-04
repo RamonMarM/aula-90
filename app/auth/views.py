4f40c8438cef6e0c2f19f10af8b8557fe80c62cd
RamonMarM <120236024+RamonMarM@users.noreply.github.com>
2024-09-04 20:29:28 -0300

    Update views.py
    dsa

--- app/auth/views.py
+++ app/auth/views.py
@@ -88,17 +88,6 @@ def confirm(token):
     return redirect(url_for('main.index'))
 
 
-@auth.route('/confirm')
-@login_required
-def resend_confirmation():
-    token = current_user.generate_confirmation_token()
-    send_email(current_user.email, 'Confirm Your Account',
-               'auth/email/confirm', user=current_user, token=token)
-    flash('A new confirmation email has been sent to you by email.')
-    flash('<p>To confirm your account <a href="' + url_for('auth.confirm', token=token, _external=True) + '">click here</a></p>')
-    return redirect(url_for('main.index'))
-
-
 @auth.route('/change-password', methods=['GET', 'POST'])
 @login_required
 def change_password():
